1dd9acc81074639753952fd37dd5d7791d27e58b
Stefan Ionescu <stefan_alter_ego@protonmail.com>
2021-05-03 02:59:29 +0100

    Implemented pool

--- src/ProtocolTokenLenderFirstResort.sol
+++ src/ProtocolTokenLenderFirstResort.sol
@@ -1,17 +1,51 @@
+/// StakedTokenAuctionHouse.sol
+
+// Copyright (C) 2021 Reflexer Labs, INC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pragma solidity 0.6.7;
 
+import "./utils/ReentrancyGuard.sol";
+
 abstract contract TokenLike {
+    function decimals() virtual public view returns (uint8);
+    function totalSupply() virtual public view returns (uint256);
+    function balanceOf(address) virtual public view returns (uint256);
+    function mint(address, uint) virtual public;
+    function burn(address, uint) virtual public;
     function approve(address, uint256) virtual external returns (bool);
+    function transfer(address, uint256) virtual external returns (bool);
     function transferFrom(address,address,uint256) virtual external returns (bool);
 }
 abstract contract AuctionHouseLike {
-    function startAuction(uint256) external returns (uint256 id);
+    function activeStakedTokenAuctions() virtual public view returns (uint256);
+    function startAuction(uint256) virtual external returns (uint256);
 }
 abstract contract AccountingEngineLike {
-
+    function debtAuctionBidSize() virtual public view returns (uint256);
+    function unqueuedUnauctionedDebt() virtual public view returns (uint256);
+}
+abstract contract SAFEEngineLike {
+    function coinBalance(address) virtual public view returns (uint256);
+    function debtBalance(address) virtual public view returns (uint256);
+}
+abstract contract RewardDripperLike {
+    function dripReward() virtual external;
 }
 
-contract ProtocolTokenLenderFirstResort {
+contract ProtocolTokenLenderFirstResort is ReentrancyGuard {
     // --- Auth ---
     mapping (address => uint) public authorizedAccounts;
     /**
@@ -49,27 +83,38 @@ contract ProtocolTokenLenderFirstResort {
     // --- Variables ---
     // Flag that allows/blocks joining
     bool      public canJoin;
+    // Flag that indicates whether canPrintProtocolTokens can ignore auctioning ancestor tokens
+    bool      public bypassAuctions;
+    // Whether the contract allows forced exits or not
+    bool      public forcedExit;
+    // Last block when a reward was pulled
+    uint256   public lastRewardBlock;
     // The current delay enforced on an exit
     uint256   public exitDelay;
     // Time during which an address can exit without requesting a new window
     uint256   public exitWindow;
-    // Total amount staked
-    uint256   public totalStaked;
-    // Min maount of staked tokens that must remain in the contract and not be auctioned
+    // Min maount of ancestor tokens that must remain in the contract and not be auctioned
     uint256   public minStakedTokensToKeep;
-
-    // Amounts staked by each address
-    mapping(address => uint256)    public staked;
+    // Amount of ancestor tokens to auction at a time
+    uint256   public tokensToAuction;
+    // Minimum amount of tokens to auction
+    uint256   public minTokensToAuction;
 
     // Exit data
     mapping(address => ExitWindow) public exitWindows;
 
-    // Token being staked
-    TokenLike            public stakedToken;
+    // The token being deposited in the pool
+    TokenLike            public ancestor;
+    // The token being backed by ancestor tokens
+    TokenLike            public descendant;
     // Auction house for staked tokens
     AuctionHouseLike     public auctionHouse;
     // Accounting engine contract
     AccountingEngineLike public accountingEngine;
+    // The safe engine contract
+    SAFEEngineLike       public safeEngine;
+    // Contract that drips rewards
+    RewardDripperLike    public rewardDripper;
 
     // Max delay that can be enforced for an exit
     uint256 public immutable MAX_DELAY;
@@ -78,5 +123,304 @@ contract ProtocolTokenLenderFirstResort {
     // Max exit window during which an address can exit without waiting again for another window
     uint256 public immutable MAX_EXIT_WINDOW;
 
+    // --- Events ---
+    event AddAuthorization(address account);
+    event RemoveAuthorization(address account);
+    event ModifyParameters(bytes32 indexed parameter, uint256 data);
+    event ModifyParameters(bytes32 indexed parameter, address data);
+    event ToggleJoin(bool canJoin);
+    event ToggleBypassAuctions(bool bypassAuctions);
+    event ToggleForcedExit(bool forcedExit);
+    event AuctionAncestorTokens(address auctionHouse, uint256 amount);
+    event RequestExit(address indexed account, uint256 start, uint256 end);
+    event Join(address indexed account, uint256 price, uint256 amount);
+    event Exit(address indexed account, uint256 price, uint256 amount);
 
+    constructor(
+      address ancestor_,
+      address descendant_,
+      address auctionHouse_,
+      address accountingEngine_,
+      address safeEngine_,
+      address rewardDripper_,
+      uint256 maxDelay_,
+      uint256 minExitWindow_,
+      uint256 maxExitWindow_,
+      uint256 exitDelay_,
+      uint256 exitWindow_,
+      uint256 minStakedTokensToKeep_,
+      uint256 tokensToAuction_,
+      uint256 minTokensToAuction_
+    ) public {
+        require(maxDelay_ > 0, "ProtocolTokenLenderFirstResort/null-max-delay");
+        require(both(maxExitWindow_ > 0, maxExitWindow_ > minExitWindow_), "ProtocolTokenLenderFirstResort/invalid-max-exit-window");
+        require(minExitWindow_ > 0, "ProtocolTokenLenderFirstResort/invalid-min-exit-window");
+        require(exitDelay_ <= maxDelay_, "ProtocolTokenLenderFirstResort/invalid-exit-delay");
+        require(both(exitWindow_ >= minExitWindow_, exitWindow_ <= maxExitWindow_), "ProtocolTokenLenderFirstResort/invalid-exit-window");
+        require(minStakedTokensToKeep_ > 0, "ProtocolTokenLenderFirstResort/null-min-staked-tokens");
+        require(tokensToAuction_ > 0, "ProtocolTokenLenderFirstResort/null-tokens-to-auction");
+        require(both(minTokensToAuction_ > 0, minTokensToAuction_ < tokensToAuction_), "ProtocolTokenLenderFirstResort/invalid-min-tokens-to-auction");
+        require(auctionHouse_ != address(0), "ProtocolTokenLenderFirstResort/null-auction-house");
+        require(accountingEngine_ != address(0), "ProtocolTokenLenderFirstResort/null-accounting-engine");
+        require(safeEngine_ != address(0), "ProtocolTokenLenderFirstResort/null-safe-engine");
+        require(rewardDripper_ != address(0), "ProtocolTokenLenderFirstResort/null-reward-dripper");
+
+        authorizedAccounts[msg.sender] = 1;
+        canJoin                        = true;
+
+        MAX_DELAY                      = maxDelay_;
+        MIN_EXIT_WINDOW                = minExitWindow_;
+        MAX_EXIT_WINDOW                = maxExitWindow_;
+
+        exitDelay                      = exitDelay_;
+        exitWindow                     = exitWindow_;
+
+        minStakedTokensToKeep          = minStakedTokensToKeep_;
+        tokensToAuction                = tokensToAuction_;
+        minTokensToAuction             = minTokensToAuction_;
+
+        auctionHouse                   = AuctionHouseLike(auctionHouse_);
+        accountingEngine               = AccountingEngineLike(accountingEngine_);
+        safeEngine                     = SAFEEngineLike(safeEngine_);
+        rewardDripper                  = RewardDripperLike(rewardDripper_);
+
+        ancestor                       = TokenLike(ancestor_);
+        descendant                     = TokenLike(descendant_);
+
+        require(ancestor.decimals() == 18, "ProtocolTokenLenderFirstResort/ancestor-decimal-mismatch");
+        require(descendant.decimals() == 18, "ProtocolTokenLenderFirstResort/descendant-decimal-mismatch");
+
+        emit AddAuthorization(msg.sender);
+    }
+
+    // --- Boolean Logic ---
+    function both(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := and(x, y)}
+    }
+    function either(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := or(x, y)}
+    }
+
+    // --- Math ---
+    uint256 public constant WAD = 10 ** 18;
+
+    function addition(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, "ProtocolTokenLenderFirstResort/add-overflow");
+    }
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "ProtocolTokenLenderFirstResort/mul-overflow");
+    }
+    function wdivide(uint x, uint y) internal pure returns (uint z) {
+        require(y > 0, "ProtocolTokenLenderFirstResort/wdiv-by-zero");
+        z = multiply(x, WAD) / y;
+    }
+    function wmultiply(uint x, uint y) internal pure returns (uint z) {
+        z = multiply(x, y) / WAD;
+    }
+
+    // --- Administration ---
+    /*
+    * @notify Switch between allowing and disallowing joins
+    */
+    function toggleJoin() external isAuthorized {
+        canJoin = !canJoin;
+        emit ToggleJoin(canJoin);
+    }
+    /*
+    * @notify Switch between ignoring and taking into account auctions in canPrintProtocolTokens
+    */
+    function toggleBypassAuctions() external isAuthorized {
+        bypassAuctions = !bypassAuctions;
+        emit ToggleBypassAuctions(bypassAuctions);
+    }
+    /*
+    * @notify Switch between allowing exits when the system is underwater or blocking them
+    */
+    function toggleForcedExit() external isAuthorized {
+        forcedExit = !forcedExit;
+        emit ToggleForcedExit(forcedExit);
+    }
+    /*
+    * @notify Modify an uint256 parameter
+    * @param parameter The name of the parameter to modify
+    * @param data New value for the parameter
+    */
+    function modifyParameters(bytes32 parameter, uint256 data) external isAuthorized {
+        if (parameter == "exitDelay") {
+          require(data <= MAX_DELAY, "ProtocolTokenLenderFirstResort/invalid-exit-delay");
+          exitDelay = data;
+        }
+        else if (parameter == "exitWindow") {
+          require(both(data >= MIN_EXIT_WINDOW, data <= MAX_EXIT_WINDOW), "ProtocolTokenLenderFirstResort/invalid-exit-window");
+          exitWindow = data;
+        }
+        else if (parameter == "minStakedTokensToKeep") {
+          require(data > 0, "ProtocolTokenLenderFirstResort/null-min-staked-tokens");
+          minStakedTokensToKeep = data;
+        }
+        else if (parameter == "tokensToAuction") {
+          require(both(data > 0, data > minTokensToAuction), "ProtocolTokenLenderFirstResort/invalid-tokens-to-auction");
+          tokensToAuction = data;
+        }
+        else if (parameter == "minTokensToAuction") {
+          require(both(data > 0, data < tokensToAuction), "ProtocolTokenLenderFirstResort/invalid-min-tokens-to-auction");
+          minTokensToAuction = data;
+        }
+        else revert("ProtocolTokenLenderFirstResort/modify-unrecognized-param");
+        emit ModifyParameters(parameter, data);
+    }
+    /*
+    * @notify Modify an address parameter
+    * @param parameter The name of the parameter to modify
+    * @param data New value for the parameter
+    */
+    function modifyParameters(bytes32 parameter, address data) external isAuthorized {
+        require(data != address(0), "ProtocolTokenLenderFirstResort/null-data");
+
+        if (parameter == "auctionHouse") {
+          auctionHouse = AuctionHouseLike(data);
+        }
+        else if (parameter == "accountingEngine") {
+          accountingEngine = AccountingEngineLike(data);
+        }
+        else if (parameter == "rewardDripper") {
+          rewardDripper = RewardDripperLike(data);
+        }
+        else revert("ProtocolTokenLenderFirstResort/modify-unrecognized-param");
+        emit ModifyParameters(parameter, data);
+    }
+
+    // --- Getters ---
+    /*
+    * @notify Return the ancestor token balance for this contract
+    */
+    function depositedAncestor() public view returns (uint256) {
+        return ancestor.balanceOf(address(this));
+    }
+    /*
+    * @notify Returns how many ancestor tokens are offered for one descendant token
+    */
+    function ancestorPerDescendant() public view returns (uint256) {
+        return descendant.totalSupply() == 0 ? WAD : wdivide(depositedAncestor(), descendant.totalSupply());
+    }
+    /*
+    * @notify Returns how many descendant tokens are offered for one ancestor token
+    */
+    function descendantPerAncestor() public view returns (uint256) {
+        return descendant.totalSupply() == 0 ? WAD : wdivide(descendant.totalSupply(), depositedAncestor());
+    }
+    /*
+    * @notify Given a custom amount of ancestor tokens, it returns the corresponding amount of descendant tokens to mint when someone joins
+    * @param wad The amount of ancestor tokens to compute the descendant tokens for
+    */
+    function joinPrice(uint256 wad) public view returns (uint256) {
+        return wmultiply(wad, descendantPerAncestor());
+    }
+    /*
+    * @notify Given a custom amount of descendant tokens, it returns the corresponding amount of ancestor tokens to send when someone exits
+    * @param wad The amount of descendant tokens to compute the ancestor tokens for
+    */
+    function exitPrice(uint256 wad) public view returns (uint256) {
+        return wmultiply(wad, ancestorPerDescendant());
+    }
+
+    /*
+    * @notice Returns whether the protocol is underwater or not
+    */
+    function protocolUnderwater() public view returns (bool) {
+        uint256 unqueuedUnauctionedDebt = accountingEngine.unqueuedUnauctionedDebt();
+
+        return both(
+          accountingEngine.debtAuctionBidSize() <= unqueuedUnauctionedDebt,
+          safeEngine.coinBalance(address(accountingEngine)) < unqueuedUnauctionedDebt
+        );
+    }
+
+    /*
+    * @notice Returns whether the pool can auction ancestor tokens
+    */
+    function canAuctionTokens() public view returns (bool) {
+        return both(
+          protocolUnderwater(),
+          addition(minStakedTokensToKeep, minTokensToAuction) <= depositedAncestor()
+        );
+    }
+
+    /*
+    * @notice Returns whether the system can mint new ancestor tokens
+    */
+    function canPrintProtocolTokens() public view returns (bool) {
+        return both(
+          !canAuctionTokens(),
+          either(auctionHouse.activeStakedTokenAuctions() == 0, bypassAuctions)
+        );
+    }
+
+    // --- Core Logic ---
+    /*
+    * @notify Pull ancestor token rewards from the dripper
+    */
+    function pullReward() public nonReentrant {
+        if (either(block.number == lastRewardBlock, protocolUnderwater())) return;
+        rewardDripper.dripReward();
+    }
+
+    /*
+    * @notify Create a new auction that sells ancestor tokens in exchange for system coins
+    */
+    function auctionAncestorTokens() external nonReentrant {
+        require(canAuctionTokens(), "ProtocolTokenLenderFirstResort/cannot-auction-tokens");
+
+        uint256 selfAncestorBalance = depositedAncestor();
+        uint256 amountToSell        = (addition(minStakedTokensToKeep, tokensToAuction) <= selfAncestorBalance) ?
+                                      tokensToAuction : minTokensToAuction;
+
+        ancestor.approve(address(auctionHouse), amountToSell);
+        auctionHouse.startAuction(amountToSell);
+
+        emit AuctionAncestorTokens(address(auctionHouse), amountToSell);
+    }
+
+    /*
+    * @notify Join ancestor tokens in exchange for descendant tokens
+    * @param wad The amount of ancestor tokens to join
+    */
+    function join(uint256 wad) public {
+        require(both(canJoin, !protocolUnderwater()), "ProtocolTokenLenderFirstResort/join-not-allowed");
+        require(wad > 0, "ProtocolTokenLenderFirstResort/null-ancestor-to-join");
+
+        uint256 price = joinPrice(wad);
+        require(price > 0, "ProtocolTokenLenderFirstResort/null-join-price");
+
+        require(ancestor.transferFrom(msg.sender, address(this), wad), "ProtocolTokenLenderFirstResort/could-not-transfer-ancestor");
+        descendant.mint(msg.sender, price);
+
+        pullReward();
+
+        emit Join(msg.sender, price, wad);
+    }
+    /*
+    * @notice Request a new exit window during which you can burn descendant tokens in exchange for ancestor tokens
+    */
+    function requestExit() public {
+        require(now > exitWindows[msg.sender].end, "ProtocolTokenLenderFirstResort/ongoing-request");
+        exitWindows[msg.sender].start = addition(now, exitDelay);
+        exitWindows[msg.sender].end   = addition(exitWindows[msg.sender].start, exitWindow);
+        emit RequestExit(msg.sender, exitWindows[msg.sender].start, exitWindows[msg.sender].end);
+    }
+    /*
+    * @notify Burn descendant tokens in exchange for getting ancestor tokens from this contract
+    * @param wad The amount of descendant tokens to exit/burn
+    */
+    function exit(uint256 wad) public nonReentrant {
+        require(wad > 0, "ProtocolTokenLenderFirstResort/null-descendant-to-burn");
+        require(both(both(now >= exitWindows[msg.sender].start, now <= exitWindows[msg.sender].end), exitWindows[msg.sender].end > 0), "ProtocolTokenLenderFirstResort/not-in-window");
+        require(either(!protocolUnderwater(), forcedExit), "ProtocolTokenLenderFirstResort/exit-not-allowed");
+
+        uint256 price = exitPrice(wad);
+
+        require(ancestor.transfer(msg.sender, price), "ProtocolTokenLenderFirstResort/could-not-transfer-ancestor");
+        descendant.burn(msg.sender, wad);
+        emit Exit(msg.sender, price, wad);
+    }
 }
